8e49ecfbc40394b438afabc6948e8139750f9db0
yuta <yuta20030801@icloud.com>
2017-07-30 00:24:42 +0900

    add 通知機能

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const Discord = require("discord.js"),
 			client  = new Discord.Client(),
 			mongo   = require('mongodb').MongoClient,
-			cronJob = require('cron').CronJob;
+			cronJob = require('cron').CronJob,
+			status  = {
+									online: 'オンライン',
+									offline: 'オフライン',
+									idle: '退席中',
+									dnd: 'Do not Disturb'
+								};
 
 mongo.connect('mongodb://localhost:27017/online', (error, db) => {
 	var users = db.collection('users');
@@ -16,7 +22,9 @@ mongo.connect('mongodb://localhost:27017/online', (error, db) => {
 			});
 		});
 		client.on('presenceUpdate', (oldMember, newMember) => {
+			if (oldMember.presence.status==newMember.presence.status) return;
 			users.updateOne({id: newMember.user.id}, {$set: {date: new Date().getTime()}});
+			client.channels.get('336901767226720256').send(`${newMember.user.username} さんが ${status[newMember.presence.status]} になりました。`);
 		});
 	});
 
